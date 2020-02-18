270da70b8ec139d24dc6e9597c6501b00032895d
sarahcrox <croxford.sarahm@gmail.com>
2020-02-17 20:04:39 -0700

    JS Stalker Complete

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Hello, Sarah!");
+var userName = prompt("What is your first name?");
+var userNameTwo = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+var fullName = userName + userNameTwo;
+console.log("Your full name is " + fullName);
+console.log("Your age is " + userAge);
